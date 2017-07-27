2bf4acf6a4c51bfdbece40aac0930034b3b01d9e
Ruslan <yesinruslan@gmail.com>
2017-07-27 14:43:26 +0300

    Перенесено завантаження ресурсів в GameScreen1, переробив побудову прикладу

--- core/src/kopach/fast/math/MyGameClass.java
+++ core/src/kopach/fast/math/MyGameClass.java
@@ -12,7 +12,6 @@ public class MyGameClass extends Game {
     public SpriteBatch spriteBatch;
     public BitmapFont gs_text_to_btn, gs_text_vidp, gs_text_pryklad, gs_text_score, gs_text_text_best_score, gs_text_best_score, gs_text_time, text_restart_t_s, text_restart_t_b_s, text_restart_s, text_restart_b_s;
     public BitmapFont score_textlevel, score_t_b_s, score_b_s;
-    public BitmapFont gs1_text_text_score, gs1_text_score, gs1_text_text_best_score, gs1_text_best_score, gs1_text_time, gs1_text_text_no_prav_vidp, gs1_text_no_prav_vidp, gs1_text_pryklad, gs1_text_vidp_right, gs1_text_vidp_wrong, gs1_text_btn;
     public BitmapFont gs2_text_text_score, gs2_text_score, gs2_text_text_best_score, gs2_text_best_score, gs2_text_time, gs2_text_pryklad, gs2_text_vidp, gs2_text_znak;
     public BitmapFont gs3_text_text_score, gs3_text_score, gs3_text_time, gs3_text_text_best_score, gs3_text_best_score, gs3_text_btn;
 
@@ -33,7 +32,7 @@ public class MyGameClass extends Game {
 
         bitmapFont();
 
-        this.setScreen(new MenuScreen(this));
+        this.setScreen(new GameScreen1(this));
     }
 
     public void render() {
@@ -72,29 +71,6 @@ public class MyGameClass extends Game {
         score_textlevel.getData().setScale(1.5f, 1.5f);
         score_t_b_s.getData().setScale(1.5f, 1.5f);
         score_b_s.getData().setScale(1.1f, 1.1f);
-
-        gs1_text_text_score = new BitmapFont(Gdx.files.internal("bitmapfont/black bold 70.fnt"), Gdx.files.internal("bitmapfont/black bold 70.png"), false);
-        gs1_text_score = new BitmapFont(Gdx.files.internal("bitmapfont/green bold 70.fnt"), Gdx.files.internal("bitmapfont/green bold 70.png"), false);
-        gs1_text_text_best_score = new BitmapFont(Gdx.files.internal("bitmapfont/black bold 70.fnt"), Gdx.files.internal("bitmapfont/black bold 70.png"), false);
-        gs1_text_best_score = new BitmapFont(Gdx.files.internal("bitmapfont/red bold 70.fnt"), Gdx.files.internal("bitmapfont/red bold 70.png"), false);
-        gs1_text_time = new BitmapFont(Gdx.files.internal("bitmapfont/black bold 70.fnt"), Gdx.files.internal("bitmapfont/black bold 70.png"), false);
-        gs1_text_text_no_prav_vidp = new BitmapFont(Gdx.files.internal("bitmapfont/black bold 70.fnt"), Gdx.files.internal("bitmapfont/black bold 70.png"), false);
-        gs1_text_no_prav_vidp = new BitmapFont(Gdx.files.internal("bitmapfont/red bold 70.fnt"), Gdx.files.internal("bitmapfont/red bold 70.png"), false);
-        gs1_text_pryklad = new BitmapFont(Gdx.files.internal("bitmapfont/black bold 70.fnt"), Gdx.files.internal("bitmapfont/black bold 70.png"), false);
-        gs1_text_vidp_right = new BitmapFont(Gdx.files.internal("bitmapfont/green bold 70.fnt"), Gdx.files.internal("bitmapfont/green bold 70.png"), false);
-        gs1_text_vidp_wrong = new BitmapFont(Gdx.files.internal("bitmapfont/red bold 70.fnt"), Gdx.files.internal("bitmapfont/red bold 70.png"), false);
-        gs1_text_btn = new BitmapFont(Gdx.files.internal("bitmapfont/white bold 70.fnt"), Gdx.files.internal("bitmapfont/white bold 70.png"), false);
-        gs1_text_text_score.getData().setScale(0.5f, 0.5f);
-        gs1_text_score.getData().setScale(0.6f, 0.6f);
-        gs1_text_text_best_score.getData().setScale(0.5f, 0.5f);
-        gs1_text_best_score.getData().setScale(0.6f, 0.6f);
-        gs1_text_time.getData().setScale(0.7f, 0.7f);
-        gs1_text_text_no_prav_vidp.getData().setScale(0.5f, 0.5f);
-        gs1_text_no_prav_vidp.getData().setScale(0.6f, 0.6f);
-        gs1_text_pryklad.getData().setScale(1.4f, 1.4f);
-        gs1_text_vidp_right.getData().setScale(1.6f, 1.6f);
-        gs1_text_vidp_wrong.getData().setScale(1.5f, 1.5f);
-        gs1_text_btn.getData().setScale(0.8f, 0.8f);
 
         gs2_text_text_score = new BitmapFont(Gdx.files.internal("bitmapfont/black bold 70.fnt"), Gdx.files.internal("bitmapfont/black bold 70.png"), false);
         gs2_text_score = new BitmapFont(Gdx.files.internal("bitmapfont/green bold 70.fnt"), Gdx.files.internal("bitmapfont/green bold 70.png"), false);
